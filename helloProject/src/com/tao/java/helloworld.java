0175c11bbb391996c4d271c88a8d690f6e9054a0
ToTmeng <zt1873705423@126.com>
2019-12-19 23:09:46 +0800

    将java工程添加到版本库

---
+++ helloProject/src/com/tao/java/helloworld.java
@@ -0,0 +1,12 @@
+package com.tao.java;
+
+/**
+ * @author tao
+ * @date 2019/12/19-23:00
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello,GIT");
+    }
+}
